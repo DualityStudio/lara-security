4edc5727aa11e6f83e4faa927a4db9b2fd0627fb
Thomas Palmer <thomas.palmer.developer@gmail.com>
2024-04-23 17:51:20 +0100

    Dont apply to binary responses

--- src/SecurityHeaders.php
+++ src/SecurityHeaders.php
@@ -4,6 +4,7 @@ namespace DualityStudio\LaraSecurity;
 
 use Closure;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Illuminate\Support\Collection;
 
 class SecurityHeaders
@@ -11,6 +12,10 @@ class SecurityHeaders
     public function handle(Request $request, Closure $next)
     {
         $response = $next($request);
+
+        if ($response instanceof BinaryFileResponse) {
+            return $response;
+        }
 
         if (config('lara-security.enabled')) {
             Headers::getHeaders()
